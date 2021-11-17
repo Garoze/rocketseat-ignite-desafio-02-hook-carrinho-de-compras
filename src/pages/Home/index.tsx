ead5c20f865b4a9f2edd1004e049af07efe1319d
Garoze <marcelogaroze@hotmail.com>
2021-11-17 19:12:28 -0300

    feat: Home and cart amount

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
 
-import { ProductList } from "./styles";
 import { api } from "../../services/api";
-import { formatPrice } from "../../util/format";
 import { useCart } from "../../hooks/useCart";
+
+import { formatPrice } from "../../util/format";
+
+import { ProductList } from "./styles";
 
 interface Product {
   id: number;
@@ -25,9 +27,10 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount;
+    return sumAmount;
+  }, {} as CartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
@@ -46,7 +49,6 @@ const Home = (): JSX.Element => {
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
     addProduct(id);
   }
 
@@ -64,7 +66,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 0
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
